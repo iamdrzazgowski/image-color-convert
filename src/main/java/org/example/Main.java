a45e0711fc30bec9ee1e19d45fd41e3bca3d4416
iamdrzazgowski <iamdrzazgowski@gmail.com>
2023-06-07 09:15:59 +0200

    threads - editing image

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,13 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String path = "D:\\Jet Brains\\Java Projects\\threads\\src\\zdj.jpg";
         imageManagement managementImage = new imageManagement();
         managementImage.readImage(path);
-        managementImage.increaseBrightness(50);
+//        managementImage.increaseBrightness(50);
+        managementImage.addBrightnessWithThreads(50);
         managementImage.writeImage(path);
-        System.out.println(path);
 
     }
 }
--- src/main/java/org/example/imageManagement.java
+++ src/main/java/org/example/imageManagement.java
@@ -8,16 +8,18 @@ import java.io.IOException;
 
 public class imageManagement {
     private BufferedImage image;
+    private int height, width;
 
     public void readImage(String path) throws IOException {
         File imageFile = new File(path);
         image = ImageIO.read(imageFile);
+        height = image.getHeight();
+        width = image.getWidth();
     }
 
     public void writeImage(String path) throws IOException {
         File imageFile = new File(path);
         String format = path.substring(path.lastIndexOf(".") + 1);
-        System.out.println(format);
         ImageIO.write(image, format, imageFile);
     }
 
@@ -44,4 +46,45 @@ public class imageManagement {
 
         System.out.println("Jasność obrazu zwiększona o " + wartosc);
     }
+
+    public void addBrightnessWithThreads(int wartosc) throws InterruptedException {
+        int cores = Runtime.getRuntime().availableProcessors();
+        int chunk = height / cores;
+        Thread threads[] = new Thread[cores];
+        for (int i = 0; i < cores; ++i) {
+            int threadIndex = i;
+            threads[i] = new Thread(() -> {
+                int startline = threadIndex * chunk;
+                int endline = (threadIndex == cores - 1) ?
+                        height :
+                        startline + chunk;
+                for (int y = startline; y < endline; y++) {
+                    for (int x = 0; x < width; x++) {
+                        Color kolor = new Color(image.getRGB(x, y));
+                        int red = kolor.getRed() + wartosc;
+                        int green = kolor.getGreen() + wartosc;
+                        int blue = kolor.getBlue() + wartosc;
+
+                        red = Math.max(0, Math.min(255, red));
+                        green = Math.max(0, Math.min(255, green));
+                        blue = Math.max(0, Math.min(255, blue));
+
+                        Color nowyKolor = new Color(red, green, blue);
+                        image.setRGB(x, y, nowyKolor.getRGB());
+                    }
+                }
+            });
+        }
+
+        for (int j = 0; j < cores; ++j) {
+            threads[j].start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
 }
+
+
+
+
